8a0d824e9c0c7b50aed74a28b42d5d0db19867e3
Angel Johank <angeljohank@gmail.com>
2023-09-19 22:03:31 -0400

    unused classes removed

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,7 @@
 import { type Component, JSX, createEffect } from 'solid-js'
 import { createStore } from 'solid-js/store'
 import { logIn, createUser, isLoggedIn } from './backend/user'
-import { useNavigate } from '@solidjs/router'
+import { A, useNavigate } from '@solidjs/router'
 
 const Login: Component = () => {
   // check if the user is logged in
@@ -29,11 +29,11 @@ const Login: Component = () => {
   }
 
   return (
-    <section class="overflow-hidden bg-gray-900">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 animate-fade-up">
-        <a
-          href="#"
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+    <section class="h-screen overflow-hidden bg-gray-900">
+      <div class="flex flex-col items-center justify-center h-full px-6 py-8 animate-fade-up">
+        <A
+          href="/"
+          class="flex items-center mb-6 text-2xl font-semibold text-white"
         >
           <img
             class="w-8 h-8 mr-2"
@@ -41,17 +41,17 @@ const Login: Component = () => {
             alt="logo"
           />
           Minilink
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        </A>
+        <div class="rounded-lg border bg-gray-800 border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
               Inicia sesión
             </h1>
             <div class="space-y-4 md:space-y-6">
               <div>
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-white"
                 >
                   Usuario
                 </label>
@@ -60,14 +60,14 @@ const Login: Component = () => {
                   name="alias"
                   id="email"
                   onInput={onInput}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="usuario@random.com"
                 />
               </div>
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-white"
                 >
                   Contraseña
                 </label>
@@ -77,7 +77,7 @@ const Login: Component = () => {
                   id="password"
                   placeholder="••••••••"
                   onInput={onInput}
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="border m:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                 />
               </div>
 
@@ -85,14 +85,14 @@ const Login: Component = () => {
                 <button
                   type="button"
                   onClick={() => logIn(credentials)}
-                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
                 >
                   Iniciar Sesión
                 </button>
                 <button
                   type="button"
                   onClick={() => createUser(credentials)}
-                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800"
                 >
                   Registrarse
                 </button>
